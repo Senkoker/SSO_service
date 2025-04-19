2b82855203fe3fdea9e3549212143dd8747c0884
Senko Minin <swilkfo@gmail.com>
2025-04-19 14:40:34 +0300

    add dockerfile & docker compose

--- main/migrations/main.go
+++ main/migrations/main.go
@@ -2,28 +2,26 @@ package main
 
 import (
 	"database/sql"
-	"flag"
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/ilyakaznacheev/cleanenv"
 	_ "github.com/lib/pq"
 	"log"
 )
 
-//команда для запуска go run main.go --database_path=postgres://postgres:Masterchyrka890@localhost:5432/postgres?sslmode=disable --migration_path=C:/Golang_social_project/GRPC_Service_sso/internal/migration_db
+type Migration struct {
+	StoragePath    string `env:"STORAGE_PATH"`
+	MigrationsPath string `env:"MIGRATIONS_PATH"`
+}
 
 func main() {
-	var storagePath, migrationsPath string
-	flag.StringVar(&storagePath, "database_path", "", "indicates the path of db")
-	flag.StringVar(&migrationsPath, "migration_path", "", "indicates the path of migrations")
-	flag.Parse()
-	if storagePath == "" {
-		panic("migration is reqiure")
+	var migration Migration
+	err := cleanenv.ReadEnv(&migration)
+	if err != nil {
+		log.Fatal(err)
 	}
-	if migrationsPath == "" {
-		panic("migrations-path is required")
-	}
-	db, err := sql.Open("postgres", storagePath)
+	db, err := sql.Open("postgres", migration.StoragePath)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -32,7 +30,7 @@ func main() {
 		log.Fatal(err)
 	}
 	m, err := migrate.NewWithDatabaseInstance(
-		"file://"+migrationsPath,
+		"file://"+migration.MigrationsPath,
 		"postgres", driver)
 	if err != nil {
 		log.Fatal(err)
